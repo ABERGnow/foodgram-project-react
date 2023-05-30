f2f2e7c691f6b4168c17a4a36e05e971809aaa60
Абдурасул Эргашев <rasul.8913@gmail.com>
2023-05-30 14:49:56 +0300

    one more

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -3,7 +3,6 @@ import os
 from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
