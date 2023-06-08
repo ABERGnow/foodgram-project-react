5d03b1305ab23250344ce17a519ab8a23bd7d2ed
Абдурасул Эргашев <rasul.8913@gmail.com>
2023-06-08 10:24:18 +0300

    one more

--- backend/api/views/recipes.py
+++ backend/api/views/recipes.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
 from django.db.models import Count
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from reportlab.pdfbase import pdfmetrics, ttfonts
